571eb5a8312eb0b2b02ddf0a9e803145586cde3b
ncu2023 <aaronbeango@gmail.com>
2023-12-04 16:00:11 +0800

    finish filter sample

--- src/main/java/com/example/myspring/filter/MyFilter.java
+++ src/main/java/com/example/myspring/filter/MyFilter.java
@@ -1,17 +1,27 @@
 package com.example.myspring.filter;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.myspring.model.BaseResponseModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebFilter(urlPatterns = "/*", filterName = "myFilter")
 public class MyFilter extends OncePerRequestFilter {
+    // 白名單
+    final ArrayList<String> whiteListArrayList = new ArrayList<>(Arrays.asList(new String[]{"/login"}));
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -19,8 +29,28 @@ public class MyFilter extends OncePerRequestFilter {
         System.out.println("My Filter getServletPath: " + request.getServletPath());
         System.out.println("My Filter getRequestURL: " + request.getRequestURL());
 
-        // 放行，到下一個filter或是api本體
-        filterChain.doFilter(request, response);
+        if(whiteListArrayList.contains(request.getServletPath())) {
+            // 不需要過濾
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 判斷有無登入
+        HttpSession session = request.getSession();
+
+        String username = (String)session.getAttribute("login");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if(username == null) {
+            // 未登入，直接response
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("utf-8");
+            response.getWriter().println(objectMapper.writeValueAsString(new BaseResponseModel(999, "未登入(filter)")));
+        } else {
+            // 放行，到下一個filter或是api本體
+            filterChain.doFilter(request, response);
+        }
     }
     
 }
