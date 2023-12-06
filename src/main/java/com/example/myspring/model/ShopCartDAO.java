1e60c542491a21a096dcb49ea10275f42069974d
ncu2023 <aaronbeango@gmail.com>
2023-12-06 09:28:33 +0800

    fix id issue

--- src/main/java/com/example/myspring/model/ShopCartDAO.java
+++ src/main/java/com/example/myspring/model/ShopCartDAO.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -14,12 +16,13 @@ import lombok.Data;
 @Table(name = "shop_cart")
 public class ShopCartDAO {
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+        
     int prdId;
 
     int quantity;
 
-    @CreatedDate
     LocalDateTime createdDate;
 
     int userId;
